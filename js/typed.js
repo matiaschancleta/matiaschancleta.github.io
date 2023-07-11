a637ebbe2511d05d7da1120cabfa2d468bb8ed6c
Matias Rueda <ruedamatiasg@gmail.com>
2023-07-10 23:48:09 -0300

    finished v1

---
+++ js/typed.js
@@ -0,0 +1,7 @@
+
+var typed2 = new Typed(".typed-txt", {
+    strings: ["Programming Technician", "Front-end Developer", "human (I think)"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+})
