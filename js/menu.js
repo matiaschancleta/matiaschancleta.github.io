ac9a8ba862c5dc4c5db59f47b20027868dfb64e0
Matias Rueda <ruedamatiasg@gmail.com>
2023-07-14 11:17:16 -0300

    menu fixed v2

--- js/menu.js
+++ js/menu.js
@@ -3,17 +3,20 @@ const openIcon = document.querySelector('.open-icon');
 const closeIcon = document.querySelector('.close-icon')
 const navbar = document.querySelector('.navbar');
 
-// when click in the menu icon, it changes to a close icon and shows the navbar (for thinner screens)
+// show/hide the menu when the user clicks in the menu icon
 
 menu.addEventListener('click', () => {
     navbar.classList.toggle('show');
 });
 
-window.addEventListener('mousedown', e => {
+// close the menu when the user clicks anywhere or scrolls while the menu is open
+
+const hideMenu = e => {
     if (navbar.classList.contains('show')
         && e.target != navbar && e.target != openIcon && e.target != closeIcon) {
         navbar.classList.toggle("show");
     }
+}
 
-    return console.log(e)
-})
+window.addEventListener('click', hideMenu)
+window.addEventListener('touchmove', hideMenu)
